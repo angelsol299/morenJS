f9cc9852563197c2db4c5ede0ff6d28e8d266bd8
angel <angelsol299@hotmail.com>
2019-04-11 22:54:15 +0200

    adding more questions

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const correctAnswers = ["B", "A", "C", "A"];
+const correctAnswers = ["B", "A", "C", "D", "A"];
 const form = document.querySelector(".quiz-form");
 const result = document.querySelector(".result");
 
@@ -9,13 +9,14 @@ form.addEventListener("submit", e => {
     form.q1.value,
     form.q2.value,
     form.q3.value,
-    form.q4.value
+    form.q4.value,
+    form.q5.value
   ];
 
   //Check answers
   userAnswers.forEach((answer, index) => {
     if (answer === correctAnswers[index]) {
-      score += 25;
+      score += 20;
     }
   });
   //show result on page
